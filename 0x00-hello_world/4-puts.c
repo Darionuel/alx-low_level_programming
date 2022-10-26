c650508bf286ae028f5f3135208e04c78f496337
darionuel <emmanuelogunekod@gmail.com>
2022-10-26 08:40:05 +0000

    edited comment and removed new lined

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * prints out a strimg
+ * main - Entry point
  *
- * return value is 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
