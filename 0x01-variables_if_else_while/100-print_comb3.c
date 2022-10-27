fb231c3a583671c6cfdbfc35e4120eb5131d28c7
darionuel <emmanuelogunekod@gmail.com>
2022-10-27 19:06:19 +0000

    trying to get correct output

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,14 +17,17 @@ int main(void)
 	{
 		while (j <= '9')
 		{
-			putchar(i);
-			putchar(j);
-			j++;
-
-			if (i <= '9' || j <= '9')
+			if (i < j)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(i);
+				putchar(j);
+
+				if (i != '8' || (i == '8' && j != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				j++;
 			}
 		}
 		i++;
