aa233d3e4145d89670e97ef6f84171031b41e96b
darionuel <emmanuelogunekod@gmail.com>
2022-10-27 13:07:12 +0000

    removed trailing whitespace in line 7 and made line 8 shorter

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - print the last digit of the number stored in the variable n
- * 
- * The program assigns a random number to the variable n each time it is executed
+ * main - print the last digit of the number stored in the variable n.
+ *
+ * assigns a random number to the variable n each time it is executed
  *
  * Return: Always 0 (Success)
  */
