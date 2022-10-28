24a5586b16bf5f002a3b1e3a853e899486c1bdc7
darionuel <emmanuelogunekod@gmail.com>
2022-10-28 13:04:55 +0000

    added code to check for all alphabets

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,13 +3,13 @@
 /**
  * _isalpha - checks for alphabetic character
  *
- * @c - letter being checked
+ * @c: letter being checked
  *
  * Return: Always 0 (Success)
  */
 int _isalpha(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 	{
 		return (1);
 	}
