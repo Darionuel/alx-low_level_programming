24c45f87181cbe314f5c5eb34a545d4d7420a597
darionuel <emmanuelogunekod@gmail.com>
2022-11-07 15:07:12 +0000

    trying to fix error output

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,35 +6,34 @@ void times_table(void)
 {
 	int i = 0;
 
-	while (i <= 9)
+	while (i < 10)
 	{
 		int j = 0;
 
-		while (i <= 9)
+		while (i < 10)
 		{
 			int k = (j * i);
 
 			if (j == 0)
 			{
 				_putchar(k + '0');
-				_putchar(',');
-				_putchar(' ');
 			}
-			else if (k < 10)
+			else if (k >= 10)
 			{
-				_putchar(k + '0');
 				_putchar(',');
 				_putchar(' ');
+				_putchar(k + '0');
 			}
 			else
 			{
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
 				_putchar(',');
 				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 			j++;
 		}
-	i++;
+		_putchar('\n');
+		i++;
 	}
 }
