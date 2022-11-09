8f0a5c73b9d6bb1bd79008a692c4339d7413d43a
darionuel <emmanuelogunekod@gmail.com>
2022-11-09 01:55:11 +0000

    reduced the number of lines to comply with betty coding style and adjusted the edge case of the first if statement

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_times_table: prints the n times table, starting with 0.
+ * print_times_table - prints the n times table, starting with 0.
  * If n is greater than 15 or less than 0,
  * the function does not print anything.
  *
@@ -12,7 +12,7 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n < 15 && n > 0)
+	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -22,24 +22,21 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar('0');
-				}
-				else if (k >= 10 && k < 100)
+				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
+				} else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
-				}
-				else
+				} else
 				{
 					_putchar(',');
 					_putchar(' ');
