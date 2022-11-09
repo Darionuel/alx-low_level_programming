2a1635b8dbb62afeafcb1eb6abbec66e0368e651
darionuel <emmanuelogunekod@gmail.com>
2022-11-09 00:34:11 +0000

    removed trailing whitespace on line 6 and 12

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,13 +3,13 @@
 /**
  * print_to_98 - prints all natural numbers from n to 98,
  * followed by a new line
- * 
+ *
  * @n: integer to start printing from
  */
 void print_to_98(int n)
 {
 	int i;
-	
+
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
