cc729d03c2b0fd35de1a7c1962831dabcfb97ef0
darionuel <emmanuelogunekod@gmail.com>
2022-11-15 13:19:41 +0000

    program that finds and prints the sum of the even-valued terms that do not exceed 4,000,000, followed by a new line

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - finds and prints the sum of the
+ * even-valued terms, followed by a new line.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	long int i, j, k, sum, next;
+	
+	j = 1;
+	k = 2;
+	sum = 0;
+	
+	for (i = 1; i <= 37; i++)
+	{
+		if ((j % 2 == 0) && (j < 4000000))
+		{
+			sum += j;
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
