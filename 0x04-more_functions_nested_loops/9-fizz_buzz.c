9bd6a1ee4f0ec44abd3d70a3d3097f74090ab1a2
darionuel <emmanuelogunekod@gmail.com>
2022-11-22 00:05:16 +0000

    trying to fix error

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
@@ -28,15 +28,18 @@ int main(void)
 		{
 			printf("Buzz ");
 		}
-		else if (i == 100)
-		{
-			printf("Buzz");
-		}
+		/**
+		 * else if (i == 100)
+		 * {
+		 *	printf("Buzz");
+		 * }
+		 */
 		else
 		{
 			printf("%d ", i);
 		}
 	}
+	printf("Buzz");
 	printf("\n");
 	return (0);
 }
